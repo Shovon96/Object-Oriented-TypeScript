82f8c7c30b3c0b5faa9f87840e7e9773fa219a31
Shovon96 <fakhruddinshovon@gmail.com>
2025-05-07 01:45:43 +0600

    Encapsulation added

---
+++ src/encapsulation.ts
@@ -0,0 +1,26 @@
+{
+    // Encapsulation mane hocche kono gopon kora/privet kora/hidden kora.
+
+    class BankAccount {
+        private readonly id: number;
+        public name: string;
+        protected _balance: number;
+
+        constructor(id: number, name: string, balance: number) {
+            this.id = id,
+            this.name = name,
+            this._balance = balance
+        }
+
+    depositBalance (ammount: number) {
+       return this._balance = this._balance + ammount;
+    }        
+
+    }
+
+    const person1 = new BankAccount (3234, 'Mr. X', 100);
+    // console.log(person1.balance)
+    const getDepositBalance = person1.depositBalance(200);
+    console.log(getDepositBalance);
+
+}
